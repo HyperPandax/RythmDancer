e5237f31af562f32291f89094d4fe4a29fd375a4
HyperPandax <lisannereilman@gmail.com>
2020-05-24 18:43:00 +0200

    cleanup enz

--- Assets/Scripts/Game/BeatScroller.cs
+++ Assets/Scripts/Game/BeatScroller.cs
@@ -3,27 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BeatScroller : MonoBehaviour { 
-    [SerializeField] private float beatTempo;
-    [SerializeField] private bool hasStarted;
+    [SerializeField] private float _beatTempo;
+    [SerializeField] private bool _hasStarted;
 
-    [SerializeField] private float noteSpeed;
+    [SerializeField] private float _noteSpeed;
+
+    private List<NoteObject> _notes;
+    private NoteObject _note;
 
     // Start is called before the first frame update
     void Start(){
-        beatTempo = beatTempo / 60f;
-        
+        _beatTempo = _beatTempo / 60f;
+        _notes = new List<NoteObject>();
+        _note = Resources.Load<NoteObject>("Prefabs/Note");
+        SpawnNote(this.transform.position.x);
     }
 
     // Update is called once per frame
     void Update(){
-        if (!hasStarted){
+        if (!_hasStarted){
            
         }else{
-            transform.position -= (new Vector3(0f, 0f, (beatTempo * noteSpeed) * Time.deltaTime));
+            for(var i = 0; i< _notes.Count; i++)
+            {
+                _notes[i].transform.position -= (new Vector3(0f, 0f, (_beatTempo * _noteSpeed) * Time.deltaTime));
+
+            }
         }
     }
     
     public void setHasStarted(bool hasstarted){
-        hasStarted = hasstarted;
+        _hasStarted = hasstarted;
+    }
+    public void SpawnNote(float xPos)
+    {
+        NoteObject n = Instantiate(_note, new Vector3(xPos, 1.0f, 30), Quaternion.Euler(0, 0, 0));
+        _notes.Add(n);
     }
 }
--- Assets/Scripts/Game/NoteObject.cs
+++ Assets/Scripts/Game/NoteObject.cs
@@ -3,6 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NoteObject : MonoBehaviour{
+
+    void Update()
+    {
+
+        // Move the object forward along its z axis 5 unit/second.
+        // 20 units total
+        
+        //if song started
+        //transform.Translate(Vector3.back * Time.deltaTime * 5);
+
+        
+    }
     private void OnTriggerEnter(Collider other){
         if (other.tag == "Missed"){
             gameObject.SetActive(false);
